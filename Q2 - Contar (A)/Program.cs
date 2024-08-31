2c7161120e71239255ebd1ba2d712f058170312f
Vitor Onofre Ramos <107143879+VitorOnofreRamos@users.noreply.github.com>
2024-08-31 14:13:41 -0300

    second commit

---
+++ Q2 - Contar (A)/Program.cs
@@ -0,0 +1,19 @@
+﻿//Contar a letra "A"
+Console.Write("Digite aqui: ");
+string input = Console.ReadLine();
+
+var result = CountLetterA(input);
+
+Console.WriteLine($"Letra 'a' encontrada: {result.found}");
+Console.WriteLine($"Quantidade de ocorrências: {result.count}");
+
+static (bool found, int count) CountLetterA(string str)
+{
+    string lowerStr = str.ToLower();
+
+    int count = lowerStr.Count(c => c == 'a');
+
+    bool found = count > 0;
+
+    return (found, count);
+}
---
+++ Q3 - SOMA/Program.cs
@@ -0,0 +1,8 @@
+﻿int INDICE = 12, SOMA = 0, K = 1; 
+
+while (K < INDICE)
+{
+    K = K + 1; SOMA = SOMA + K; 
+}
+
+Console.WriteLine($"O valor da SOMA é igual a: {SOMA}");
